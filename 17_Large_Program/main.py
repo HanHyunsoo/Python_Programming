97965e9cada6603661dfb311eb0d8ad3db294b2e
HanHyunsoo <gustn4563@gmail.com>
2018-07-19 01:10:44 +0900

    Large Program

---
+++ 17_Large_Program/main.py
@@ -0,0 +1,15 @@
+# main.py
+
+import cat
+
+# 2개의 cat 인스턴스 생성
+romeo = cat.Cat("Romeo")
+juliet = cat.Cat("Juliet")
+
+# 로미오와 놀아준다.
+romeo.speak()
+romeo.drink()
+
+# 줄리엣과 놀아준다.
+juliet.speak()
+juliet.drink()
