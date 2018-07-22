e3e1e6f07d2e0b7a708ee442627483b2b9be5f48
HanHyunsoo <gustn4563@gmail.com>
2018-07-23 00:46:25 +0900

    Ping Pong Game Program

--- 19_MyPong/main.py
+++ 19_MyPong/main.py
@@ -6,6 +6,9 @@ import table, ball, bat
 # 전역 변수 선언
 x_velocity = 10
 y_velocity = 10
+score_left = 0
+score_right = 0
+first_serve = True
 
 # tkinter 공장으로부터 윈도우 주문
 window = Tk()
@@ -26,13 +29,62 @@ bat_R = bat.Bat(table = my_table, width = 15, height = 100,
 
 #### 함수
 def game_flow():
-    # 공이 배트에 닿으면
+    global first_serve
+    global score_left
+    global score_right
+    
+    # 첫번째 서브를 기다립니다.
+    if (first_serve == True):
+        my_ball.stop_ball()
+        first_serve = False
+    
+    # 배트가 공을 받아치는지 확인
     bat_L.detect_collision(my_ball)
     bat_R.detect_collision(my_ball)
-    
+
+    # 왼쪽 벽에서 공이 부딪치는지 감지
+    if (my_ball.x_posn <= 3):
+        my_ball.stop_ball()
+        my_ball.start_position()
+        bat_L.start_position()
+        bat_R.start_position()
+        my_table.move_item(bat_L.rectangle, 20 ,150, 35, 250)
+        my_table.move_item(bat_R.rectangle, 575 ,150, 590, 250)
+        score_left = score_left + 1
+        if (score_left >= 10):
+            score_left = "Win"
+            score_right = "Lose"
+        first_serve = True
+        my_table.draw_score(score_left, score_right)
+        
+    # 오른쪽 벽에서 공이 부딪치는지 감지
+    if (my_ball.x_posn + my_ball.width >= my_table.width -3):
+        my_ball.stop_ball()
+        my_ball.start_position()
+        bat_L.start_position()
+        bat_R.start_position()
+        my_table.move_item(bat_L.rectangle, 20 ,150, 35, 250)
+        my_table.move_item(bat_R.rectangle, 575 ,150, 590, 250)
+        score_right = score_right + 1
+        if (score_right >= 10):
+            score_left = "Lose"
+            score_right = "Win"
+        first_serve = True
+        my_table.draw_score(score_left, score_right)
+        
     my_ball.move_next()
     window.after(50, game_flow)
 
+# restart_game 함수는 여기에 추가
+def restart_game(master):
+    global score_left
+    global score_right
+    my_ball.start_ball(x_speed = x_velocity, y_speed = 0)
+    if (score_left == "Win" or score_left == "Lose"):
+        score_left = 0
+        score_right = 0
+    my_table.draw_score(score_left, score_right)
+    
 # 배트를 제어하기 위해 키보드의 키에 연결
 # 왼쪽 배트
 window.bind("W", bat_L.move_up)
@@ -43,6 +95,9 @@ window.bind("s", bat_L.move_down)
 # 오른쪽 배트
 window.bind("<Up>", bat_R.move_up)
 window.bind("<Down>", bat_R.move_down)
+
+# 스페이스바를 눌러 게임 재시작
+window.bind("<space>", restart_game)
 
 # game_flow를 호출합니다.
 game_flow()
